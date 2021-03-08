64a004b4301a942ad91fc2af5796836370091387
jrn2778 <jrn2778@rit.edu>
2021-03-08 01:41:45 -0500

    Styled the suggest page

--- client/src/suggest.js
+++ client/src/suggest.js
@@ -1,53 +1,62 @@
-// Initialization after window loads
-window.onload = () => {
-  // Change default submit action for suggestion form
-  document.querySelector('#suggestionForm').onsubmit = sendSuggestionForm;
-};
+import { customNav } from './vueComponents.js';
 
 // Response after form action has completed
 function handleFormResponse(e) {
   const xhr = e.target;
-  const content = document.querySelector('#status');
 
   switch (xhr.status) {
     case 200:
-      content.textContent = 'Success!';
+      alert('Success!');
       break;
     case 201:
-      content.textContent = 'Created!';
+      alert('Created!');
       break;
     case 204:
-      content.textContent = 'Updated (No Content)!';
+      alert('Updated (No Content)!');
       break;
     case 400:
-      content.textContent = 'Bad Request!';
+      alert('Bad Request!');
       break;
     default:
-      content.textContent = 'Error code not implemented by client';
+      alert('Error code not implemented by client');
   }
 }
 
-// Submit action for the suggestion form
-function sendSuggestionForm(e) {
-  e.preventDefault();
+// Initialize the Vue instance
+const suggest = new Vue({
+  el: '#suggest',
+  components: {
+    customNav,
+  },
+  data: function () {
+    return {};
+  },
+  methods: {
+    // Submit action for the suggestion form
+    sendSuggestionForm: function (e) {
+      const form = e.target;
+      const formAction = form.getAttribute('action');
+      const formMethod = form.getAttribute('method');
 
-  const form = e.target;
-  const formAction = form.getAttribute('action');
-  const formMethod = form.getAttribute('method');
+      // Get the action
+      const goodActionField = form.querySelector('#goodAction');
+      let formData = `action=${goodActionField.value}`;
+      goodActionField.value = '';
 
-  const goodActionField = form.querySelector('#goodAction');
+      // Get the tags
+      let checked = form.querySelectorAll('input[type="checkbox"]:checked');
+      checked.forEach((c) => {
+        formData += `&tags=${c.name}`;
+        c.checked = false;
+      });
 
-  let formData = `action=${goodActionField.value}`;
-  let checked = form.querySelectorAll('input[type="checkbox"]:checked');
-  checked.forEach((c) => {
-    formData += `&tags=${c.name}`;
-  });
+      const xhr = new XMLHttpRequest();
+      xhr.open(formMethod, formAction);
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      xhr.onload = handleFormResponse;
+      xhr.send(formData);
 
-  const xhr = new XMLHttpRequest();
-  xhr.open(formMethod, formAction);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onload = handleFormResponse;
-  xhr.send(formData);
-
-  return false;
-}
+      return false;
+    },
+  },
+});
